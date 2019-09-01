ffc0c1d61df735ba7cff72849a65c461b014aca1
saifulss <saifulss85@gmail.com>
2019-09-01 20:30:41 +0800

    Change top-level key for auth.

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,9 @@
 import {combineReducers} from "redux";
-import {userReducer} from "./user/reducers";
 import {authReducer} from "./auth/reducers";
 
 export const rootReducer = combineReducers({
-  user: userReducer,
-  token: authReducer,
+  // user: userReducer,
+  auth: authReducer,
 });
 
 export type AppState = ReturnType<typeof rootReducer>
