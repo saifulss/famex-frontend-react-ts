663cb83bc0b77f68bed4c0918bce3cdc39a5553c
saifulss <saifulss85@gmail.com>
2019-09-01 09:01:18 +0800

    Add more explicit typings.

--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -2,11 +2,13 @@ import axios from "axios";
 import {ApiConstants} from "../../constants/ApiConstants";
 import {User} from "../user/types";
 import {STORE_ACCESS_TOKEN, STORE_CURRENT_USER, StoreAccessTokenAction, StoreCurrentUserAction} from "./types";
-import {Dispatch} from "redux";
+import {AnyAction} from "redux";
+import {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {AppState} from "../rootReducer";
 
 // thunk
-export const attemptAuthentication = (username: string, password: string) => {
-  return (dispatch: Dispatch): void => {
+export const attemptAuthentication = (username: string, password: string): ThunkAction<void, any, any, AnyAction> => {
+  return (dispatch: ThunkDispatch<AppState, any, AnyAction>) => {
     axios.post(`${ApiConstants.BASE_URL}/authenticate`, null, {
       params: {
         username,
