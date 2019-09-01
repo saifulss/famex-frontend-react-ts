8b9ddbaa908ae66fe5127a565bc00c67b5501be8
saifulss <saifulss85@gmail.com>
2019-09-01 21:07:41 +0800

    Make AuthenticatedBaseView kick users to login screen when user is not logged in.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - implement topnav toolbar showing current user's name
 - implement kickout to home when no access token in local storage
 - implement prettier and eslint
+- implement "memorize which route the user was at prior to kicking him to login screen"
 - do a full redux-thunk flow, using real backend API, with thinly sliced SRP-ed actions
 - add types to parts of code using "any"
 - incorporate use of redux-actions
--- src/views/AuthenticatedBaseView.tsx
+++ src/views/AuthenticatedBaseView.tsx
@@ -1,26 +1,30 @@
 import * as React from 'react';
 import {connect} from "react-redux";
 import {attemptAuthentication} from "../store/auth/actions";
+import {AppState} from "../store/rootReducer";
+import {User} from "../store/user/types";
+import {Redirect} from "react-router";
 
 interface BaseAuthenticatedViewProps {
   component: React.ReactElement;
+  currentUser?: User;
   attemptAuthentication: (username: string, password: string) => void;  // todo: extract to its own interface
 }
 
-class BaseAuthenticatedBaseView extends React.Component<BaseAuthenticatedViewProps> {
-  componentDidMount(): void {
-    this.props.attemptAuthentication("saifulss85@gmail.com", "secret");
-  }
+const BaseAuthenticatedBaseView = (props: BaseAuthenticatedViewProps) => {
+  if (props.currentUser === undefined) return <Redirect to="/login"/>;
 
-  render() {
-    return (
-      <div>
-        {this.props.component}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {props.component}
+    </div>
+  );
+};
+
+const mapStateToProps = (state: AppState) => ({
+  currentUser: state.auth.currentUser
+});
 
 const mapDispatchToProps = {attemptAuthentication};
 
-export const AuthenticatedBaseView = connect(null, mapDispatchToProps)(BaseAuthenticatedBaseView);
+export const AuthenticatedBaseView = connect(mapStateToProps, mapDispatchToProps)(BaseAuthenticatedBaseView);
