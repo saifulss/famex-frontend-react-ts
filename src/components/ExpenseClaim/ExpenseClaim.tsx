ee8467530443d7bc6db08bddf4654e9ea12334ad
saifulss <saifulss85@gmail.com>
2021-04-01 12:35:30 +0800

    Do away with user portrait and secondary actions for list items

--- src/components/ExpenseClaim/ExpenseClaim.tsx
+++ src/components/ExpenseClaim/ExpenseClaim.tsx
@@ -1,13 +1,7 @@
 import * as React from "react";
 import { ExpenseClaim as ExpenseClaimModel } from "../../store/expenseClaim/types";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import FolderIcon from "@material-ui/icons/Folder";
 import ListItemText from "@material-ui/core/ListItemText";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import IconButton from "@material-ui/core/IconButton";
 import ListItem from "@material-ui/core/ListItem";
-import DeleteIcon from "@material-ui/icons/Delete";
 import { CurrencyUtils } from "../../utils/CurrencyUtils";
 import moment from "moment";
 
@@ -65,20 +59,10 @@ const renderDescriptionIfExists = (expenseClaim: ExpenseClaimModel) => {
 
 export const ExpenseClaim = (props: ExpenseClaimProps) => (
   <ListItem alignItems="flex-start">
-    <ListItemAvatar>
-      <Avatar>
-        <FolderIcon />
-      </Avatar>
-    </ListItemAvatar>
     <ListItemText
       primary={renderPrimary(props.expenseClaim)}
       secondary={renderSecondary(props.expenseClaim)}
       disableTypography={true}
     />
-    <ListItemSecondaryAction>
-      <IconButton edge="end" aria-label="delete">
-        <DeleteIcon />
-      </IconButton>
-    </ListItemSecondaryAction>
   </ListItem>
 );
