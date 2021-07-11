f25f40614b4a27ca7e206949f7cab96eb9465e88
saifulss <saifulss85@gmail.com>
2021-07-11 22:05:00 +0800

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 yarn build
-docker build -t famex:latest .
-docker tag famex:latest 979548132360.dkr.ecr.ap-southeast-1.amazonaws.com/famex-react:latest
+docker build -t famex-react:latest .
+docker tag famex-react:latest 979548132360.dkr.ecr.ap-southeast-1.amazonaws.com/famex-react:latest
 docker push 979548132360.dkr.ecr.ap-southeast-1.amazonaws.com/famex-react:latest
