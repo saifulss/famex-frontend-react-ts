99a4f77f4fe207e1619c034b50b5b66171ebd174
saifulss <saifulss85@gmail.com>
2021-07-11 23:54:00 +0800

    Round off instead of trunc

--- src/store/expenseClaimForm/actions.ts
+++ src/store/expenseClaimForm/actions.ts
@@ -40,7 +40,7 @@ export function submitExpenseClaim(): any {
     await axios.post(
       `${ApiConstants.BASE_URL}/expense-claims`,
       {
-        amount_in_cents: Math.trunc(amount * 100),
+        amount_in_cents: Math.round(amount * 100),
         description,
         claimant: getState().auth.currentUser!!.id
       },
