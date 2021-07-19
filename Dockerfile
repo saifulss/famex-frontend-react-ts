d6406169f09f96b71b3032ed84cfd1c334e1e82d
saifulss <saifulss85@gmail.com>
2021-07-19 18:51:31 +0800

    Add Nginx files for LetsEncrypt

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM nginx:1.21.1
 COPY build /usr/share/nginx/html
 
 COPY ./famex.conf /etc/nginx/conf.d/default.conf
+
+COPY ./nginx.conf /etc/nginx/nginx.conf
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 yarn build
 docker build -t famex-react:latest .
-docker tag famex-react:latest 979548132360.dkr.ecr.ap-southeast-1.amazonaws.com/famex-react:latest
-docker push 979548132360.dkr.ecr.ap-southeast-1.amazonaws.com/famex-react:latest
+docker tag famex-react:latest ${AWS_ECR_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/famex-react:latest
+aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ECR_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+docker push ${AWS_ECR_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/famex-react:latest
