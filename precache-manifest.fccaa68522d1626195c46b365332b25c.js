a5b05b4de9877973e94eceede8b7385a5def65bf
saifulss <saifulss85@gmail.com>
2021-08-02 12:19:40 +0800

    Updates

---
+++ precache-manifest.fccaa68522d1626195c46b365332b25c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8d8c0b285ec44c00105ce229c70c38",
+    "url": "/famex-frontend-react-ts/index.html"
+  },
+  {
+    "revision": "c235f70ab98b6cb328cd",
+    "url": "/famex-frontend-react-ts/static/css/main.b67dec76.chunk.css"
+  },
+  {
+    "revision": "2269c0eef65180c35013",
+    "url": "/famex-frontend-react-ts/static/js/2.8f0a2911.chunk.js"
+  },
+  {
+    "revision": "e5ec2407327402ed3c0d11c4cb0a5d30",
+    "url": "/famex-frontend-react-ts/static/js/2.8f0a2911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c235f70ab98b6cb328cd",
+    "url": "/famex-frontend-react-ts/static/js/main.261cd604.chunk.js"
+  },
+  {
+    "revision": "e603bde3f7fcc0577f8f",
+    "url": "/famex-frontend-react-ts/static/js/runtime-main.df626722.js"
+  }
+]);
