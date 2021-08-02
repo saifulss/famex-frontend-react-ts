c6949bf9c9ce1101eec8bc5ff459183c14de4ac4
saifulss <saifulss85@gmail.com>
2021-08-02 23:37:13 +0800

    Remove port

--- src/constants/ApiConstants.ts
+++ src/constants/ApiConstants.ts
@@ -1,5 +1,5 @@
 const host = process.env.REACT_APP_STRAPI_HOST ? process.env.REACT_APP_STRAPI_HOST : 'http://localhost';
-const BASE_URL = `${host}:1337`;
+const BASE_URL = `${host}`;
 
 export const ApiConstants = {
   BASE_URL
